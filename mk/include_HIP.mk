fb534047a45278245d39b1a3260043628674a6cd
Jan Eitzinger <jan@moebiusband.org>
2025-11-24 20:18:29 +0100

    Create GPU portability header for CUDA/HIP toolchain support

---
+++ mk/include_HIP.mk
@@ -0,0 +1,12 @@
+CC   = hipcc
+LD   = $(CC)
+
+VERSION   = --version
+HIPFLAGS  = --offload-arch=native
+CPUFLAGS  = -O3 -pipe
+CFLAGS    = -O3 $(HIPFLAGS) $(CPUFLAGS)
+LFLAGS    =
+DEFINES   = -D_GNU_SOURCE
+DEFINES   += -D__HIP_PLATFORM_AMD__
+INCLUDES  =
+LIBS      =
--- src/kernels.cu
+++ src/kernels.cu
@@ -1,5 +1,4 @@
-#include <cuda_runtime.h>
-#include <curand_kernel.h>
+#include "portability.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,32 +10,30 @@ extern "C" {
 #include "timing.h"
 #include "util.h"
 
-static int getSharedMemSize(
-    int THREAD_BLOCK_SIZE, int thread_blocks_per_sm, const void *func);
+static int getSharedMemSize(int THREAD_BLOCK_SIZE, int thread_blocks_per_sm,
+                            const void *func);
 static void setBlockSize(void);
 }
 
-#define GPU_ERROR(ans)                                                                   \
-  do {                                                                                   \
-    gpuAssert((ans), __FILE__, __LINE__, true);                                          \
+#define GPU_ERROR(ans)                                                         \
+  do {                                                                         \
+    gpuAssert((ans), __FILE__, __LINE__, true);                                \
   } while (0)
 
-static inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort)
-{
+static inline void gpuAssert(cudaError_t code, const char *file, int line,
+                             bool abort) {
   if (code != cudaSuccess) {
-    fprintf(stderr, "GPUassert: \"%s\" in %s:%d\n", cudaGetErrorString(code), file, line);
+    fprintf(stderr, "GPUassert: \"%s\" in %s:%d\n", cudaGetErrorString(code),
+            file, line);
     if (abort) {
       exit((int)code);
     }
   }
 }
 
-__global__ void init_constants(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    double *__restrict__ d,
-    const size_t N)
-{
+__global__ void init_constants(double *__restrict__ a, double *__restrict__ b,
+                               double *__restrict__ c, double *__restrict__ d,
+                               const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -49,13 +46,9 @@ __global__ void init_constants(double *__restrict__ a,
   d[tidx] = INIT_D;
 }
 
-__global__ void init_randoms(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    double *__restrict__ d,
-    const size_t N,
-    unsigned long long seed)
-{
+__global__ void init_randoms(double *__restrict__ a, double *__restrict__ b,
+                             double *__restrict__ c, double *__restrict__ d,
+                             const size_t N, unsigned long long seed) {
 
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -66,7 +59,7 @@ __global__ void init_randoms(double *__restrict__ a,
   // Declare and initialize RNG state
   curandState state;
   curand_init(seed, tidx, 0,
-      &state); // seed, sequence number, offset, &state
+              &state); // seed, sequence number, offset, &state
 
   a[tidx] = (double)curand_uniform(&state);
   b[tidx] = (double)curand_uniform(&state);
@@ -74,8 +67,7 @@ __global__ void init_randoms(double *__restrict__ a,
   d[tidx] = (double)curand_uniform(&state);
 }
 
-__global__ void initCuda(double *__restrict__ b, int scalar, const size_t N)
-{
+__global__ void initCuda(double *__restrict__ b, int scalar, const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -85,8 +77,8 @@ __global__ void initCuda(double *__restrict__ b, int scalar, const size_t N)
   b[tidx] = scalar;
 }
 
-__global__ void copyCuda(double *__restrict__ c, double *__restrict__ a, const size_t N)
-{
+__global__ void copyCuda(double *__restrict__ c, double *__restrict__ a,
+                         const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -96,8 +88,7 @@ __global__ void copyCuda(double *__restrict__ c, double *__restrict__ a, const s
   c[tidx] = a[tidx];
 }
 
-__global__ void updateCuda(double *__restrict__ a, int scalar, const size_t N)
-{
+__global__ void updateCuda(double *__restrict__ a, int scalar, const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -107,12 +98,9 @@ __global__ void updateCuda(double *__restrict__ a, int scalar, const size_t N)
   a[tidx] = a[tidx] * scalar;
 }
 
-__global__ void triadCuda(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    const int scalar,
-    const size_t N)
-{
+__global__ void triadCuda(double *__restrict__ a, double *__restrict__ b,
+                          double *__restrict__ c, const int scalar,
+                          const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -122,9 +110,8 @@ __global__ void triadCuda(double *__restrict__ a,
   a[tidx] = b[tidx] + (scalar * c[tidx]);
 }
 
-__global__ void daxpyCuda(
-    double *__restrict__ a, double *__restrict__ b, const int scalar, const size_t N)
-{
+__global__ void daxpyCuda(double *__restrict__ a, double *__restrict__ b,
+                          const int scalar, const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -134,12 +121,9 @@ __global__ void daxpyCuda(
   a[tidx] = a[tidx] + (scalar * b[tidx]);
 }
 
-__global__ void striadCuda(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    double *__restrict__ d,
-    const size_t N)
-{
+__global__ void striadCuda(double *__restrict__ a, double *__restrict__ b,
+                           double *__restrict__ c, double *__restrict__ d,
+                           const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -149,11 +133,8 @@ __global__ void striadCuda(double *__restrict__ a,
   a[tidx] = b[tidx] + (d[tidx] * c[tidx]);
 }
 
-__global__ void sdaxpyCuda(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    const size_t N)
-{
+__global__ void sdaxpyCuda(double *__restrict__ a, double *__restrict__ b,
+                           double *__restrict__ c, const size_t N) {
   int tidx = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tidx >= N) {
@@ -163,8 +144,7 @@ __global__ void sdaxpyCuda(double *__restrict__ a,
   a[tidx] = a[tidx] + (b[tidx] * c[tidx]);
 }
 
-__device__ void warpReduce(volatile int *shared_data, int tidx)
-{
+__device__ void warpReduce(volatile int *shared_data, int tidx) {
   shared_data[tidx] += shared_data[tidx + 32];
   shared_data[tidx] += shared_data[tidx + 16];
   shared_data[tidx] += shared_data[tidx + 8];
@@ -175,13 +155,12 @@ __device__ void warpReduce(volatile int *shared_data, int tidx)
 
 // Inspired by the
 // https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf
-__global__ void sumCuda(
-    double *__restrict__ a, double *__restrict__ a_out, const size_t N)
-{
+__global__ void sumCuda(double *__restrict__ a, double *__restrict__ a_out,
+                        const size_t N) {
   extern __shared__ int shared_data[];
 
   unsigned int tidx = threadIdx.x;
-  unsigned int i    = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
+  unsigned int i = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
   shared_data[tidx] = a[i] + a[i + blockDim.x];
   __syncthreads();
 
@@ -202,22 +181,23 @@ __global__ void sumCuda(
   }
 }
 
-#define SHARED_MEM(kernel_name)                                                          \
-  getSharedMemSize(THREAD_BLOCK_SIZE, THREAD_BLOCK_PER_SM, (const void *)&(kernel_name))
+#define SHARED_MEM(kernel_name)                                                \
+  getSharedMemSize(THREAD_BLOCK_SIZE, THREAD_BLOCK_PER_SM,                     \
+                   (const void *)&(kernel_name))
 
-#define HARNESS(kernel, kernel_name)                                                     \
-  int shared_mem_size = SHARED_MEM(kernel_name);                                         \
-  GPU_ERROR(cudaSetDevice(CUDA_DEVICE));                                                 \
-  GPU_ERROR(cudaFree(0));                                                                \
-  double S = getTimeStamp();                                                             \
-  kernel;                                                                                \
-  GPU_ERROR(cudaDeviceSynchronize());                                                    \
-  double E = getTimeStamp();                                                             \
+#define HARNESS(kernel, kernel_name)                                           \
+  int shared_mem_size = SHARED_MEM(kernel_name);                               \
+  GPU_ERROR(cudaSetDevice(CUDA_DEVICE));                                       \
+  GPU_ERROR(cudaFree(0));                                                      \
+  double S = getTimeStamp();                                                   \
+  kernel;                                                                      \
+  GPU_ERROR(cudaDeviceSynchronize());                                          \
+  double E = getTimeStamp();                                                   \
   return E - S;
 
 extern "C" {
-void allocateArrays(double **a, double **b, double **c, double **d, const size_t N)
-{
+void allocateArrays(double **a, double **b, double **c, double **d,
+                    const size_t N) {
   GPU_ERROR(cudaSetDevice(CUDA_DEVICE));
   GPU_ERROR(cudaFree(0));
 
@@ -227,12 +207,9 @@ void allocateArrays(double **a, double **b, double **c, double **d, const size_t
   GPU_ERROR(cudaMalloc((void **)d, N * sizeof(double)));
 }
 
-void initArrays(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    double *__restrict__ d,
-    const size_t N)
-{
+void initArrays(double *__restrict__ a, double *__restrict__ b,
+                double *__restrict__ c, double *__restrict__ d,
+                const size_t N) {
   GPU_ERROR(cudaSetDevice(CUDA_DEVICE));
   GPU_ERROR(cudaFree(0));
 
@@ -240,93 +217,79 @@ void initArrays(double *__restrict__ a,
 
   if (DataInitVariant == CONSTANT) {
 
-    init_constants<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE>>>(a, b, c, d, N);
+    init_constants<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE>>>(a, b, c, d,
+                                                                     N);
 
   } else if (DataInitVariant == RANDOM) {
     printf("Using random initialization.\n");
 
     unsigned long long seed = time(NULL); // unique seed
-    init_randoms<<<(N / THREAD_BLOCK_SIZE) + 1, THREAD_BLOCK_SIZE>>>(a, b, c, d, N, seed);
+    init_randoms<<<(N / THREAD_BLOCK_SIZE) + 1, THREAD_BLOCK_SIZE>>>(a, b, c, d,
+                                                                     N, seed);
   }
 
   GPU_ERROR(cudaDeviceSynchronize());
 }
 
-double init(double *__restrict__ b, double scalar, const size_t N)
-{
-  HARNESS((initCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, 0>>>(b, scalar, N)),
-      initCuda)
+double init(double *__restrict__ b, double scalar, const size_t N) {
+  HARNESS((initCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, 0>>>(
+              b, scalar, N)),
+          initCuda)
 }
 
-double copy(double *__restrict__ c, double *__restrict__ a, const size_t N)
-{
-  HARNESS((copyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              c, a, N)),
-      copyCuda)
+double copy(double *__restrict__ c, double *__restrict__ a, const size_t N) {
+  HARNESS((copyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                      shared_mem_size>>>(c, a, N)),
+          copyCuda)
 }
 
-double update(double *__restrict__ a, double scalar, const size_t N)
-{
-  HARNESS((updateCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              a, scalar, N)),
-      updateCuda)
+double update(double *__restrict__ a, double scalar, const size_t N) {
+  HARNESS((updateCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                        shared_mem_size>>>(a, scalar, N)),
+          updateCuda)
 }
 
-double triad(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    const double scalar,
-    const size_t N)
-{
-  HARNESS((triadCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              a, b, c, scalar, N)),
-      triadCuda)
+double triad(double *__restrict__ a, double *__restrict__ b,
+             double *__restrict__ c, const double scalar, const size_t N) {
+  HARNESS((triadCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                       shared_mem_size>>>(a, b, c, scalar, N)),
+          triadCuda)
 }
 
-double daxpy(
-    double *__restrict__ a, double *__restrict__ b, const double scalar, const size_t N)
-{
-  HARNESS((daxpyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              a, b, scalar, N)),
-      daxpyCuda)
+double daxpy(double *__restrict__ a, double *__restrict__ b,
+             const double scalar, const size_t N) {
+  HARNESS((daxpyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                       shared_mem_size>>>(a, b, scalar, N)),
+          daxpyCuda)
 }
 
-double striad(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    double *__restrict__ d,
-    const size_t N)
-{
-  HARNESS((striadCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              a, b, c, d, N)),
-      striadCuda)
+double striad(double *__restrict__ a, double *__restrict__ b,
+              double *__restrict__ c, double *__restrict__ d, const size_t N) {
+  HARNESS((striadCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                        shared_mem_size>>>(a, b, c, d, N)),
+          striadCuda)
 }
 
-double sdaxpy(double *__restrict__ a,
-    double *__restrict__ b,
-    double *__restrict__ c,
-    const size_t N)
-{
-  HARNESS((sdaxpyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE, shared_mem_size>>>(
-              a, b, c, N)),
-      sdaxpyCuda)
+double sdaxpy(double *__restrict__ a, double *__restrict__ b,
+              double *__restrict__ c, const size_t N) {
+  HARNESS((sdaxpyCuda<<<N / THREAD_BLOCK_SIZE + 1, THREAD_BLOCK_SIZE,
+                        shared_mem_size>>>(a, b, c, N)),
+          sdaxpyCuda)
 }
 
-double sum(double *__restrict__ a, const size_t N)
-{
+double sum(double *__restrict__ a, const size_t N) {
   GPU_ERROR(cudaSetDevice(CUDA_DEVICE));
   GPU_ERROR(cudaFree(0));
 
   double *al;
 
-  GPU_ERROR(cudaMalloc(
-      &al, (N + (THREAD_BLOCK_SIZE - 1)) / THREAD_BLOCK_SIZE * sizeof(double)));
+  GPU_ERROR(cudaMalloc(&al, (N + (THREAD_BLOCK_SIZE - 1)) / THREAD_BLOCK_SIZE *
+                                sizeof(double)));
 
   double start = getTimeStamp();
 
-  sumCuda<<<N / (THREAD_BLOCK_SIZE * 2) + 1,
-      THREAD_BLOCK_SIZE,
-      THREAD_BLOCK_SIZE * sizeof(double)>>>(a, al, N);
+  sumCuda<<<N / (THREAD_BLOCK_SIZE * 2) + 1, THREAD_BLOCK_SIZE,
+            THREAD_BLOCK_SIZE * sizeof(double)>>>(a, al, N);
 
   GPU_ERROR(cudaDeviceSynchronize());
 
@@ -337,8 +300,7 @@ double sum(double *__restrict__ a, const size_t N)
   return end - start;
 }
 
-void setBlockSize()
-{
+void setBlockSize() {
   cudaDeviceProp prop;
   GPU_ERROR(cudaGetDeviceProperties(&prop, 0));
 
@@ -363,9 +325,10 @@ void setBlockSize()
   THREAD_BLOCK_PER_SM = MIN(THREAD_BLOCK_PER_SM, THREADBLOCKPERSM);
 #endif
 
-  double occupancy = (((double)THREAD_BLOCK_SIZE * (double)THREAD_BLOCK_PER_SM) /
-                         (double)maxThreadsPerSM) *
-                     100;
+  double occupancy =
+      (((double)THREAD_BLOCK_SIZE * (double)THREAD_BLOCK_PER_SM) /
+       (double)maxThreadsPerSM) *
+      100;
 
   printf(HLINE);
   printf("Thread Block Size: \t %d\n", THREAD_BLOCK_SIZE);
@@ -373,12 +336,12 @@ void setBlockSize()
   printf("Occupancy: \t\t %.2f %% \n", occupancy);
 }
 
-int getSharedMemSize(int THREAD_BLOCK_SIZE, int thread_blocks_per_sm, const void *func)
-{
+int getSharedMemSize(int THREAD_BLOCK_SIZE, int thread_blocks_per_sm,
+                     const void *func) {
 
 #ifdef THREADBLOCKPERSM
   int max_active_thread_blocks = 0;
-  int shared_mem_size          = 1024;
+  int shared_mem_size = 1024;
 
   GPU_ERROR(cudaOccupancyMaxActiveBlocksPerMultiprocessor(
       &max_active_thread_blocks, func, THREAD_BLOCK_SIZE, shared_mem_size));
