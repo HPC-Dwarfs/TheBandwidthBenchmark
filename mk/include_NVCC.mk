fab6afe0132441223faacab77405356a285f03ce
Aditya Ujeniya <adityauj@gmail.com>
2025-12-11 10:51:56 +0100

    Working HIP comptaibility

--- mk/include_NVCC.mk
+++ mk/include_NVCC.mk
@@ -2,8 +2,12 @@ CC   = nvcc
 LD = $(CC)
 
 VERSION   = --version
+# For A100 GPUs, Ampere GA100 arch target
 NVCCFLAGS = -gencode arch=compute_80,code=sm_80
+#For A40 GPUs, Ampere GA102 arch target
 NVCCFLAGS += -gencode arch=compute_86,code=sm_86
+# For H100 GPUs, although compute_90a and sm_90a is recommended for Hopper Arch
+# https://docs.nvidia.com/cuda/hopper-compatibility-guide/index.html#building-applications-with-hopper-support
 NVCCFLAGS += -gencode arch=compute_90,code=sm_90 
 NVCCFLAGS += -Xcompiler -rdynamic --generate-line-info -Wno-deprecated-gpu-targets
 CPUFLAGS  = -O3 -pipe 
